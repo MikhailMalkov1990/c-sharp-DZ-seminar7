edc247392a28560a372db870b23eee1c1692a4e2
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-12-06 09:26:40 -0500

    commited 50

---
+++ exercise50/Program.cs
@@ -0,0 +1,51 @@
+﻿// Домашняя работа. Задача 50. Напишите программу, которая на вход принимает
+// позиции элемента в двумерном массиве, и возвращает
+// значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+Console.WriteLine("row number: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("row number: ");
+Console.WriteLine("column number: ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+void FindMatrix(int[,] matrix)
+{
+    if (row < matrix.GetLength(0) && column < matrix.GetLength(1))
+        Console.WriteLine($"result {matrix[row, column]}");
+    else
+        Console.WriteLine($"row: {row}, column {column} eror ");
+}
+int[,] creatMatrixRndInt = CreateMatrixRndInt(5, 7, 1, 10);
+PrintMatrix(creatMatrixRndInt);
+FindMatrix(creatMatrixRndInt);
